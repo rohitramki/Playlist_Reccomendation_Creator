7d88eb459254ed004a0d46ec36c24c7af4f1144d
Rohit Ramakrishnan <rohitramki@gmail.com>
2021-06-29 00:53:09 -0400

    changed the token

--- Recommendation_Algorithm.py
+++ Recommendation_Algorithm.py
@@ -4,7 +4,7 @@ import time
 from SpotifyClient import SpotifyClient
 
 from tqdm import tqdm
-client_secret = "BQDPa3HLjegxYOuhId0DwJuh9c-5OlgyllqVKy0nE3XaRWBvDfC9GGBqb6BDY1kvF8cQPrnJn9aHTe3LT2KqetrqeYSLCA7Qzm-PEOZumYQS-wOL1LVP685jzI_4x7-ZOK59idcjqKORfY1tDhwKGpBDPCtv8e9ZAESNEspfDqOmWUTE5L0nTKvKF8Ioqs4r3HLv5wn9dpCroNlfJ4NmybKCxroH4OvOiv-S5oAMBS_yhqt3ZQ"
+client_secret = "BQC0Tov0hpx4b8vvGHnX5DsiTK3HOtB2Fvugz_-AkkmULIUUfN7GkDvmN6FsqnKE6_gsBHLfkdgdqWAgLhel-r6Q8NSYH-q1e6-c1p_0ix_qz64ik04lpGPwbbYh8nr8q_Vs8gyY2JZKzUORvrIg7VsrRHY4BGOn9KQaQbVuw-0yZZFgAqf6HeDjXYuHsl0wvh0p4Wp5ScqhbAQ5c5Y6qMa6NJr-4_FzjHqjTpV_7L_qMI9Eaw"
 
 def main():
     #importlib.import_module("SpotifyClient")
